74d4d86de83d21ef7eaa5a22486d955f3d1d651d
JamesH <jwhusc7@gmail.com>
2022-03-24 14:36:55 -0400

    added styling to buttons and animation to delete drag area

--- assets/js/script.js
+++ assets/js/script.js
@@ -199,7 +199,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -238,6 +238,20 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
+  activate: function(){
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag")
+  },
+  deactivate: function(){
+    $(this).removeClass("dropover");
+    $(".bottom-trash").remove("bottom-trash-drag")
+  },
+  over: function(event){
+    event.target.addClass("dropover-active")
+  },
+  out: function(event){
+    event.target.removeClass("dropover-active")
+  },
   update: function(event) {
     // array to store the task data in 
     var tempArr = [];
@@ -279,6 +293,15 @@ $(".card .list-group").sortable({
 $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
+  over: function(){
+    $(".bottom-trash").addClass("bottom-trash-active");
+  },
+  drop: function(){
+    $(".bottom-trash").removeClass("bottom-trash-active");
+  },
+  out: function(){
+    $(".bottom-trash").removeClass("bottom-trash-active");
+  },
   drop: function(event, ui) {
     console.log("drop");
     ui.draggable.remove();
@@ -296,4 +319,4 @@ setInterval(function () {
   $(".card .list-group-item").each(function(index, el) {
     auditTask(el);
   });
-}, (1000 * 60) * 30));
+}, ((1000 * 60) * 30));

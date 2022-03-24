212940d71d45942e78f0ce6824adb88ca2516ed8
JamesH <jwhusc7@gmail.com>
2022-03-24 11:25:27 -0400

    added sortable JqueryUI

--- assets/js/script.js
+++ assets/js/script.js
@@ -199,4 +199,44 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
+// -------- SORTABLE UI -------- //
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  update: function(event) {
+    // array to store the task data in 
+    var tempArr = [];
 
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      console.log($(this))
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    console.log(text, date);
+    });
+    console.log($(this))
+    // trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
